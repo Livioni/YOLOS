bcbacfaf7d08df2ded4a91bdbe988e2a2a1b6451
livioni <livion_i@icloud.com>
2023-10-16 16:30:41 +0800

    添加token embedding 可视化

--- token_reuse_evaluate.py
+++ token_reuse_evaluate.py
@@ -3,6 +3,10 @@ import torch,warnings,argparse,random,os
 from tqdm.contrib import tzip
 from time import time
 from pathlib import Path
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import torchvision.transforms as transforms
+from einops import rearrange
 from PIL import Image
 import numpy as np
 from tools.patch_reuse_tools import TRANSFORM_tiny, TRANSFORM_base, MOT_CLASSES, COLORS, extract_bboxes_from_coco, rescale_bboxes, resize_bbox, detections_to_coco_format
@@ -14,6 +18,18 @@ from pycocotools.cocoeval import COCOeval
 # 忽略UserWarning
 warnings.simplefilter(action='ignore', category=UserWarning)
 
+TRANSFORM_tiny = transforms.Compose([
+            transforms.Resize((432,768)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+TRANSFORM_base = transforms.Compose([
+            transforms.Resize((608,896)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
 def get_args_parser():
     parser = argparse.ArgumentParser('Set YOLOS', add_help=False)
 
@@ -21,7 +37,7 @@ def get_args_parser():
                         help='use checkpoint.checkpoint to save mem')
     parser.add_argument('--num_class', default=1, type=int,
                         help='num of classes in the dataset')
-    parser.add_argument('--drop_proportion', default=0.1, type=float,
+    parser.add_argument('--drop_proportion', default=1.0, type=float,
                         help='the proportion of the patch to drop')
     parser.add_argument('--dataset_file', default='mot15', type=str,
                         help='the dataset to train on')
@@ -76,6 +92,76 @@ def val_dataset_preporcess(args):
             cnt = 0
     return image_paths, reuse_image_paths
 
+def visualization(image_path:str,\
+                  reuse_image_path:str,\
+                  reuse_image_bbox:list,\
+                  prediction_result:list,\
+                  output_dir:str):
+    fig, ax = plt.subplots(2, 2, figsize=(30, 20))
+    #左上放reuse image
+    reuse_image = Image.open(reuse_image_path)
+    ax[0,0].imshow(reuse_image)
+    ax[0,0].set_title('Reuse Image',fontsize=50)
+    ax[0,0].axis('off')
+    #左下放reuse image的bbox
+    ax[1,0].imshow(reuse_image)
+    for bbox in reuse_image_bbox:
+        x1, y1, x2, y2, c = bbox
+        rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, edgecolor='r', facecolor='none')
+        ax[1,0].add_patch(rect)
+        text = 'Person'
+        ax[1,0].text(x1, y1, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    ax[1,0].set_title('Reuse Image Prediction',fontsize=50)
+    #右上放image (patch-reuse)
+    image = Image.open(image_path)
+    image_copy = image.copy()
+    patch_dim_w, patch_dim_h = image.size[0] // 40, image.size[1] // 40
+    patch_num = patch_dim_h * patch_dim_w
+    all_indices = set(range(patch_num))
+    keep_patch_indices = []
+    for bbox in reuse_image_bbox:
+        x1, y1, x2, y2, c = bbox
+        # 计算与bounding box相关的patch的开始和结束索引
+        start_row_idx = y1 // (16 * 2.5)
+        start_col_idx = x1 // (16 * 2.5)
+        end_row_idx = y2 // (16 * 2.5)
+        end_col_idx = x2 // (16 * 2.5)
+        
+        # 从所有的patch中移除当前bounding box内的patch
+        for i in range(int(start_row_idx), int(end_row_idx)+1):
+            for j in range(int(start_col_idx), int(end_col_idx)+1):
+                patch_idx = i * patch_dim_w + j
+                all_indices.discard(patch_idx)
+                keep_patch_indices.append(patch_idx)
+    row = list(all_indices)
+    transformed_img_reuse = TRANSFORM_tiny.transforms[1](reuse_image)
+    transformed_img = TRANSFORM_tiny.transforms[1](image)
+    reuse_image_patches = rearrange(transformed_img_reuse, 'c (h p1) (w p2) -> (h w) (p1 p2 c)', p1=40, p2=40)
+    image_patches = rearrange(transformed_img, 'c (h p1) (w p2) -> (h w) (p1 p2 c)', p1=40, p2=40)
+    replacement_patches = reuse_image_patches[row, :]
+    image_patches[row, :] = replacement_patches
+    image = rearrange(image_patches, '(h w) (p1 p2 c) -> c (h p1) (w p2)', p1=40, p2=40, h=patch_dim_h, w=patch_dim_w)
+    image = transforms.ToPILImage()(image)
+    ax[0,1].imshow(image)
+    ax[0,1].set_title('Token Embedding Reuse Image',fontsize=50)
+    ax[0,1].axis('off')
+    #右下放image的bbox
+    ax[1,1].imshow(image_copy)
+    for bbox in prediction_result:
+        x1, y1, x2, y2, c = bbox
+        rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, edgecolor='r', facecolor='none')
+        ax[1,1].add_patch(rect)
+        text = f'Person: {c:0.2f}'
+        ax[1,1].text(x1, y1, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    ax[1,1].set_title('Token Embedding Reuse Image Prediction',fontsize=50)
+    ax[1,1].axis('off')
+
+    plt.tight_layout()
+    plt.savefig(output_dir+'/result.png')
+    return
+
 def token_reuse_inference(model, image_path : str, reuse_image_path : str, args):
     img = Image.open(image_path)
     reuse_image = Image.open(reuse_image_path)
@@ -99,12 +185,12 @@ def token_reuse_inference(model, image_path : str, reuse_image_path : str, args)
     keep_reference = probas_reference.max(-1).values > 0.9
     # convert boxes from [0; 1] to image scales
     bboxes_scaled_reference = rescale_bboxes(outputs_reference['pred_boxes'][0, keep_reference], img.size)
-    bboxex_feed_back = rescale_bboxes(outputs_reference['pred_boxes'][0, keep_reference], [input_tensor.shape[-1],input_tensor.shape[-2]])
+    bboxes_feed_back = rescale_bboxes(outputs_reference['pred_boxes'][0, keep_reference], [input_tensor.shape[-1],input_tensor.shape[-2]])
     bboxes_scale_reference_add_confidence = torch.cat((bboxes_scaled_reference, probas_reference[keep_reference]), dim=1).tolist()
     ################drop_inference#############
     with torch.no_grad():
         reuse_embedding = saved_embedding
-        reuse_region = ground_truth_bbox
+        reuse_region = bboxes_feed_back
         input_tensor = input_tensor.to(args.device)
         outputs,_,debug_data = model(input_tensor,reuse_embedding,reuse_region,args.drop_proportion)
     # keep only predictions with 0.7+ confidence
@@ -155,7 +241,9 @@ def main(args):
     model.load_state_dict(checkpoint['model'], strict=False)
     model.eval()
 
-    image_paths, reuse_image_paths = val_dataset_preporcess(args)
+    # image_paths, reuse_image_paths = val_dataset_preporcess(args)
+    image_paths = ['/home/livion/Documents/github/dataset/MOT15_coco/val/ADL-Rundle-8-000495.jpg']
+    reuse_image_paths = ['/home/livion/Documents/github/dataset/MOT15_coco/val/ADL-Rundle-8-000491.jpg']
     reference_prediction = []
     reuse_prediction = []
     reference_id_pool = []
@@ -163,6 +251,12 @@ def main(args):
     reuse_propotion = []
     for image, reuse_image in tzip(image_paths,reuse_image_paths):
         reference_bbox_c, reuse_bbox_c, reference_id, image_id, debug_data = token_reuse_inference(model,image,reuse_image,args)
+        ################visualization#############
+        visualization(image_path=image,\
+                  reuse_image_path=reuse_image,\
+                  reuse_image_bbox=reference_bbox_c,\
+                  prediction_result=reuse_bbox_c,\
+                  output_dir=args.output_dir)
         reference_prediction.append(reference_bbox_c)
         reuse_prediction.append(reuse_bbox_c)
         reference_id_pool.append(reference_id)
